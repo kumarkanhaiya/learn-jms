03e9f1aa32703c4cd9fe500c19daa24f8dbb2fee
kumarkanhaiya <kanhaiyakumarcse@gmail.com>
2020-04-02 11:51:49 +0530

    Initial project

---
+++ src/main/java/com/kanhaiya/learnjms/LearnJmsApplication.java
@@ -0,0 +1,13 @@
+package com.kanhaiya.learnjms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnJmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnJmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kanhaiya/learnjms/LearnJmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kanhaiya.learnjms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnJmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
